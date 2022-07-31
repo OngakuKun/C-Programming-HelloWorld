8a36f7cb36e30f195366e56283b624bd62e7aa39
OngakuKun <marcellehmkuhl@outlook.de>
2022-07-31 19:02:59 +0200

    Add Comment to popd

--- clean.cmd
+++ clean.cmd
@@ -8,4 +8,5 @@ REM ===   CLEAN FILES AND DIRS   ===
     del /s %OUTDIR%*.obj %OUTDIR%*.exe %OUTDIR%*.pdb %OUTDIR%*.ilk %OUTDIR%*.lib %OUTDIR%*.dll
     rmdir %INTDIR% %OUTDIR%
 
+REM ===      EXIT AND POPD       ===
     popd
